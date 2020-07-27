4e568f2e394dc7b57e5c11dce92c60d98129cf1c
TGion <tobias@gion.io>
2020-07-27 10:44:04 +0200

    Initial commit

---
+++ src/cpp-KT_lb3.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cpp-arithmetic-shift.cpp
+// Author      : Tobias Gion
+// Version     :
+// Copyright   : 
+// Description : Practice sheet LB3 from KT / Use of the shift operators
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "cpp-KT_lb3" << endl; // prints dec2bin
+	return 0;
+}
